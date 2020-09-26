c08404ce98e6f659a79fd068fc7c8b87bbd7a1fb
jonathan.earl <jonathan.earl@tylertech.com>
2020-09-25 18:43:27 -0700

    basic setup

---
+++ client/src/components/friends-feed.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<div>
+  <h1>Friends feed</h1>
+</div>
+
+<style>
+</style>
---
+++ client/src/components/search.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<div>
+  <h1>Search</h1>
+</div>
+
+<style>
+</style>
