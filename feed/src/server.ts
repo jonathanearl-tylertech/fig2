2adb5216fb3e6a98022c94a3b72ad06226f08732
jonathan earl <earl.jonathan@gmail.com>
2020-10-03 16:29:35 -0700

    add bodyparser and mongoose

--- feed/src/server.ts
+++ feed/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/posts', (req, res) => {
-    res.json({hello: 'world2'})
+app.post('/posts', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
 })
 
 app.listen(5001, () => {
