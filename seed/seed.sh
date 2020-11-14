a8e72c89770e84f2046ead2d6f97773cec56c591
whattheearl <earl.jonathan@gmail.com>
2020-11-14 13:04:48 -0800

    fix folder reference

--- seed/seed.sh
+++ seed/seed.sh
@@ -1,4 +1,4 @@
 echo seeding authors
-mongoimport 'mongodb://db' --jsonArray --db fig --collection authors --drop --file /mongo/authors.json
-mongoimport 'mongodb://db' --jsonArray --db fig --collection posts --drop --file /mongo/posts.json
+mongoimport 'mongodb://db' --jsonArray --db fig --collection authors --drop --file /seed/mongodb/authors.json
+mongoimport 'mongodb://db' --jsonArray --db fig --collection posts --drop --file /seed/mongodb/posts.json
 
