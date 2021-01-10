98a339b482eef326a88570c94b5f2ef263fc2084
whattheearl <earl.jonathan@gmail.com>
2021-01-10 13:13:44 -0800

    dockerise seed data

--- seed/seed.sh
+++ seed/seed.sh
@@ -1,4 +1,7 @@
-echo seeding authors
+#!/bin/bash
+
+echo seeding users
 mongoimport 'mongodb://db' --jsonArray --db fig --collection users --drop --file /seed/mongodb/users.json
+echo seeding posts
 mongoimport 'mongodb://db' --jsonArray --db fig --collection posts --drop --file /seed/mongodb/posts.json
 
