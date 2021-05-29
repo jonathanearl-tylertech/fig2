d82294fb3988d568e0aa236cf34a676c3ec39065
jonathan earl <earl.jonathan@gmail.com>
2021-05-29 07:14:15 -0700

    simplify

--- web-api/main.go
+++ web-api/main.go
@@ -5,47 +5,32 @@ import (
 	"log"
 	"time"
 
-	"github.com/joho/godotenv"
 	pb "github.com/whattheearl/fig/profile-service/pkg/protobuff"
 	"google.golang.org/grpc"
 )
 
 const (
-	address = "0.0.0.0:50051"
+	profileurl = "0.0.0.0:50051"
 )
 
-func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
-
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	// 	fmt.Fprintln(w, req.URL, req.Host, req.URL.Scheme)
-	// })
 
 	// http.HandleFunc("/profile", get)
 	// http.HandleFunc("/profile", post)
 
-	// server := os.ExpandEnv("${HOST}:${PORT}")
-	// fmt.Println("Serving on ", server)
-	// log.Fatal(http.ListenAndServe(server, nil))
-
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(profileurl, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
-	c := pb.NewProfileServiceClient(conn)
+	client := pb.NewProfileServiceClient(conn)
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.Get(ctx, &pb.ProfileRequest{Id: 1})
+	r, err := client.Get(ctx, &pb.ProfileRequest{Id: 1})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
