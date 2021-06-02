6e0aa0a3b547b5fa23dae33be3457a4e3fcf4a96
jonathan earl <earl.jonathan@gmail.com>
2021-06-01 19:30:40 -0700

    mv profile protobuf

--- profilesvc/protobuff/generate.sh
+++ profilesvc/protobuff/generate.sh
@@ -1,4 +1,8 @@
 #! /bin/zsh
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    --js_out=import_style=commonjs:. \
+    --grpc-web_out=import_style=typescript,mode=grpcwebtext:. \
     profile.proto
+
+
