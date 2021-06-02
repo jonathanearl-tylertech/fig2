1593e15414d048881bc591b3aab1fe854747398a
jonathan earl <earl.jonathan@gmail.com>
2021-06-01 19:32:21 -0700

    update react

--- client/src/services/post.service.ts
+++ client/src/services/post.service.ts
@@ -1,5 +1,20 @@
-class PostService {
+import { ProfileServiceClient } from '../protobuff/ProfileServiceClientPb';
+import { ProfileIdRequest } from '../protobuff/profile_pb';
+const client = new ProfileServiceClient('0.0.0.0:10000', null, null)
 
+const request = new ProfileIdRequest()
+request.setId("hellosdjfkljsdlkf");
+
+client.getById(request, {}, (err, response) => {
+  if (err) {
+    console.log(`Unexpected error for getById: code = ${err.code}` +
+                `, message = "${err.message}"`);
+  } else {
+    console.log(response.toObject());
+  }
+});
+
+class PostService {
   getAll() {
     return fetch('https://api.jonathanearl.localhost/posts')
       .then(res => res.json())
