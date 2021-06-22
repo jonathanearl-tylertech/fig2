15d9d39347ac0ead0020e165cbd62d06ade5355c
jonathan earl <earl.jonathan@gmail.com>
2021-06-21 17:20:01 -0700

    fix: handle cors for client

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -7,20 +7,28 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/rs/cors"
+
 	"github.com/whattheearl/fig/cmd/webapi/graph"
 	"github.com/whattheearl/fig/cmd/webapi/graph/generated"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
+	clientaddr := os.Getenv("CLIENT_ADDR")
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{clientaddr},
+		Debug:          true,
+	})
+	handler := c.Handler(srv)
+	port := os.Getenv("WEBAPI_PORT")
+	if port == "" {
+		panic("WEBAPI_PORT is not configured")
+	}
 	log.Printf("connect :%s for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/aws/aws-sdk-go v1.38.60 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
 	go.mongodb.org/mongo-driver v1.5.3
 	google.golang.org/grpc v1.38.0
