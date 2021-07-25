5557c7dbe6438b973bbd6dbb8c0e6e7726f3655f
jonathan earl <earl.jonathan@gmail.com>
2021-07-25 11:21:03 -0700

    feat: add theme for material-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,27 +2,43 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import FriendFeed from './components/FriendFeed';
 import NavBar from './components/NavBar'
+import { Login } from './login/Login';
 import { Profile } from './profile/Profile';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[600],
+    },
+    secondary: {
+      main: purple[300],
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <main className="h-screen w-screen">
-        <NavBar></NavBar>
-        <Switch>
-          <Route path="/feed">
-            <FriendFeed />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/">
-            <Redirect from="/" to="/profile" />
-          </Route>
-        </Switch>
-      </main>
-    </Router>
+    <ThemeProvider theme={theme}>
+
+      <Router>
+        <main className="h-screen w-screen">
+          <NavBar></NavBar>
+          <Switch>
+            <Route path="/feed">
+              <FriendFeed />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/">
+              <Redirect from="/" to="/profile" />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </ThemeProvider>
   );
 }
 
