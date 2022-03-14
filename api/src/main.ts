38af045da33fbc738988ed266b42dbb7c233f22d
Jonathan Earl <jonathan.earl@tylertech.com>
2022-03-13 18:07:02 -0700

    refactor: move data outside of api project

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,23 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import dotenv from 'dotenv';
-import { ValidationPipe } from '@nestjs/common';
 import { MongooseClient } from 'src/mongoose/mongoose-client';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 async function bootstrap() {
-  const options = { 
-    connectionString: process.env.MONGO_CONNECTIONSTRING,
-    username: process.env.MONGO_USERNAME,
-    password: process.env.MONGO_PASSWORD,
+  const options = {
+    connectionString: process.env.MONGO_CONNECTIONSTRING ?? 'mongodb://localhost:27017/fig',
+    username: process.env.MONGO_USERNAME ?? 'admin',
+    password: process.env.MONGO_PASSWORD ?? 'admin',
   };
   await MongooseClient.connect(options);
+
   const app = await NestFactory.create(AppModule);
-  
+
+  app.use(cookieParser(process.env.COOKIE_SECRET ?? "secret"));
+
   app.setGlobalPrefix('api/v1');
-  
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
---
+++ data/profiles.ts
@@ -0,0 +1,26 @@
+// import { Profile } from 'src/services/profile/profile.model';
+
+// export const ProfileSeedData: Profile[] = [
+//   {
+//     _id: '61288d28b4484d717b8ce494',
+//     username: 'whattheuser',
+//     email: 'some.emailhere@gmail.com',
+//     image: 'https://placedog.net/900/650/b',
+//     summary: 'just a small town boy, living in a hello world!',
+//     createdAt: new Date(),
+//     modifiedAt: new Date(),
+//     disabled: false,
+//   __v: 1,
+//   },
+//   {
+//     _id: '61288d28b4484d717b8ce496',
+//     username: 'whattheadmin',
+//     email: 'fake@email.com',
+//     image: 'https://placedog.net/900/650/b',
+//     summary: 'just a small town boy, living in a hello world!',
+//     createdAt: new Date(),
+//     modifiedAt: new Date(),
+//     disabled: false,
+//   __v: 1,
+//   },
+// ];
