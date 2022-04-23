796f1e110387fecfa671335d00b4ac48f2cf2cee
jonathan earl <jonathan.earl@tylertech.com>
2022-04-23 11:30:56 -0700

    chore: simplify controllers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { SessionModule } from './controllers/session/session.module';
 import { PostModule } from './services/post/post.module';
-import { RegistrationModule } from './controllers/registration/registration.module';
 import { UserModule } from './services/user/user.module';
 import { IdentityModule } from './services/identity/identity.module';
-import { ProfileModule } from './controllers/profile/profile.module';
+import { ProfileController } from './controllers/profile/profile.controller';
+import { RegistrationController } from './controllers/registration/registration.controller';
+import { SessionController } from './controllers/session/session.controller';
+import { PasswordService } from './services/password.service';
+import { EmojiGeneratorService } from './services/emoji-generator.service';
 
 @Module({
+  controllers: [
+    ProfileController,
+    RegistrationController,
+    SessionController,
+  ],
   imports: [
     IdentityModule,
     MongooseModule.forRoot('mongodb://localhost/fig'),
     PostModule,
-    RegistrationModule,
-    SessionModule,
     UserModule,
-    ProfileModule,
+  ],
+  providers: [
+    EmojiGeneratorService,
+    PasswordService,
   ]
 })
 export class AppModule { }
--- api/src/controllers/profile/profile.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from 'src/services/user/user.module';
-import { ProfileController } from './profile.controller';
-
-@Module({
-  imports: [UserModule],
-  controllers: [ProfileController]
-})
-export class ProfileModule {}
