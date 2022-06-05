6ef758592f0410c333cb0356f8f2cab13a63f5ff
Jonathan Earl <earl.jonathan@gmail.com>
2022-06-05 13:38:19 -0700

    refactor: remove cookie auth

--- api/src/controllers/profile.controller.ts
+++ api/src/controllers/profile.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Get, Param, Post, UnauthorizedException, UseGuards } from '@nestjs/common';
-import { ApiNoContentResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, UnauthorizedException } from '@nestjs/common';
+import { ApiNoContentResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from 'src/services/user.service';
 import { IconDto } from 'src/dtos/icon.dto';
 import { ProfileDto } from 'src/dtos/profile.dto';
 import { SummaryDto } from 'src/dtos/summary.dto';
-import { AuthGuard } from 'src/guards/auth.guard';
 import { Uid } from 'src/decorators/uid.decorator';
 
+@ApiTags('profile')
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly userSvc: UserService) { }
 
-  @Get(':username')
+  @Get(':uid')
   @ApiOperation({ summary: 'retrieve user profile' })
   @ApiOkResponse({ type: ProfileDto })
-  async GetProfile(@Param('username') username: string) {
-    const user = await this.userSvc.findByUsername(username);
+  async GetProfileByUsername(@Param('uid') uid: string) {
+    const user = await this.userSvc.findById(uid);
     const response: ProfileDto = {
       icon: user.icon,
       id: user._id,
@@ -25,7 +25,6 @@ export class ProfileController {
     return response;
   }
 
-  @UseGuards(AuthGuard)
   @Post(':username/summary')
   @ApiOperation({ summary: 'update user summary' })
   @ApiNoContentResponse()
@@ -41,7 +40,6 @@ export class ProfileController {
     await this.userSvc.update(user._id, { summary: data.summary });
   }
 
-  @UseGuards(AuthGuard)
   @Post(':username/icon')
   @ApiOperation({ summary: 'update users icon' })
   @ApiNoContentResponse()
