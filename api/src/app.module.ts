dde9150f30030e29af8326b1a812ed9c7474ae29
jonathan earl <earl.jonathan@gmail.com>
2022-12-23 19:25:59 -0800

    fix: missing import

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserController } from './controllers/user.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { EmojiGeneratorService } from './services/emoji-generator.service';
 
 @Module({
   controllers: [UserController],
@@ -11,5 +12,6 @@ import { User, UserSchema } from './schemas/user.schema';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forRoot('mongodb://localhost/fig'),
   ],
+  providers: [EmojiGeneratorService]
 })
 export class AppModule {}
